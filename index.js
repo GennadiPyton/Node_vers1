1825a9473738c9065419b92b0f2438ba343217f6
GennadiPyton <73903012+GennadiPyton@users.noreply.github.com>
2021-11-22 17:55:39 +0200

    Simple Web-server with pages
    comit10

--- index.js
+++ index.js
@@ -1,37 +1,58 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     if (req.method === 'GET') {
         res.writeHead(200, {
-            'Content-Type': 'text/html'
+            'Content-Type': 'text/html; charset=utf-8'
 })
-res.end(`
-    <hl>Form</hl>
-    <form method="post" action="/">
-    <input name=”title" type="text" />
-    <button type="submit">Send</button>
-</form>
-`)
- 
+
+if (req.url === '/') {
+    fs.readFile(
+        path.join(__dirname, 'views', 'index.html'),
+        'utf-8',
+        (err, content) => {
+            if (err) {
+                throw err
+}
+
+        res.end(content)
+}
+)
+} else if (req.url === '/about') {
+fs.readFile(
+    path.join(__dirname, 'views', 'about.html'),
+    'utf-8',
+    (err, content) => {
+        if (err) {
+            throw err
+}
+
+res.end(content)
+    }
+)
+}
 } else if (req.method === 'POST') {
     const body = []
     res.writeHead(200, {
-        'Content-Type': 'text/html; charset=utf-8'
-})
+    'Content-Type': 'text/html; charset=utf-8'
+    })
 
-req.on('data', data => {
-    body.push(Buffer.from(data))
-})
+    req.on('data', data => {
+        body.push(Buffer.from(data))
+    })
 
-req.on('end', () => {
+    req.on('end', () => {
     const message = body.toString().split('=')[1]
-res.end(`
-    <hl>Ваше Сообщение: ${message}</hl>
-`)
-})
+
+    res.end(`
+        <hl>Ваше сообщение: ${message}</hl>
+    `)
+    })
 }
 })
 
-server.listen(3000, () => {
+    server.listen(3000, () => {
     console.log('Server is running...')
-})
+    })  
